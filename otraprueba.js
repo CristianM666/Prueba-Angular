f49f3d9c5b6ff3e344702416426104d8f1abc005
cristianm1 <cabezasc599@gmail.com>
2024-12-17 14:20:16 -0500

    Next version fo web file.

---
+++ otraprueba.js
@@ -0,0 +1,12 @@
+(function () {
+//'use strict';
+
+var x = "Hola";
+
+Angular.module('Primerprueba', [])
+
+.controller('Primercontrol', function ($scope) {
+  $scope.name = "Cristian";
+});
+
+})();
